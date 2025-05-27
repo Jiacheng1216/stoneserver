7fabd33fbfbba10443f642333b1f702fd1a70d6b
Eric.Lin <Eric.Lin@skylinetw.com>
2025-05-27 16:49:21 +0800

    feat：解決中文檔名顯示亂碼的問題

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -8,6 +8,20 @@ cloudinary.config({
   api_secret: process.env.CLOUD_API_SECRET,
 });
 
+const fileFilter = (req, file, callback) => {
+  // 解决中文名乱码的问题 latin1 是一种编码格式
+  file.originalname = Buffer.from(file.originalname, "latin1").toString("utf8");
+  callback(null, true)
+
+  // 真正的檔案格式檢查
+  const allowedMimeTypes = ['image/jpeg', 'image/png', 'image/jpg'];
+  if (allowedMimeTypes.includes(file.mimetype)) {
+    callback(null, true);
+  } else {
+    callback(new Error('Only jpg, jpeg, and png files are allowed'), false);
+  }
+};
+
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
@@ -16,6 +30,6 @@ const storage = new CloudinaryStorage({
   },
 });
 
-const upload = multer({ storage });
+const upload = multer({ storage,fileFilter});
 
 module.exports = { cloudinary, upload };
